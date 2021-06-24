1f21b3ad707e5b1ccbdbe7948fbc1d8240ba55aa
Damian <hitoirl1@gmail.com>
2021-06-24 16:57:31 +0200

    test player

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,40 @@
 #include "opengl/window.hpp"
 #include "opengl/renderer.hpp"
 
+namespace game
+{
+	class player
+	{
+	public:
+		player(const glm::vec2& size, const glm::vec3& color) : size(size), color(color), position(15.0f, 15.0f)
+		{
+
+		}
+
+		void update(const std::shared_ptr<opengl::renderer>& renderer)
+		{
+			// todo: input (movement)
+			renderer->rect(position, size, color);
+		}
+	private:
+		const glm::vec2 size;
+		const glm::vec3 color;
+		glm::vec2 position;
+	};
+}
+
 int main()
 {
+	// opengl
 	auto window = std::make_unique<opengl::window>("ping pong");
-	auto renderer = std::make_unique<opengl::renderer>(window->getProjection());
+	auto renderer = std::make_shared<opengl::renderer>(window->getProjection());
+
+	// game
+	auto player = std::make_unique<game::player>(glm::vec2(4.0f, 125.0f), glm::vec3(1.0f, 1.0f, 1.0f));
 
 	while (window->refresh())
 	{
-		renderer->rect({ 20.0f, 20.0f }, { 16.0f, 16.0f }, { 1.0f, 1.0f, 1.0f });
+		player->update(renderer);
 		renderer->render();
 	}
 }
